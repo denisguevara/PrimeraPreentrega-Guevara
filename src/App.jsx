6aae28bb9fa3d39ecc1920c3f4b74663ca0c7d98
Denis Guevara <denis.nahuel.guevara@gmail.com>
2023-11-25 14:03:39 -0300

    commit 9

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import ItemListContainer from './components/ItemListContainer'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './components/Home.jsx'
 import Cart from './components/Cart.jsx'
-import ItemDetailContainer from './components/ItemDetailContainer'
+import ItemDetailContainer from './components/ItemDetailContainer.jsx'
 
 //ejecutar npm install react-router-dom
 const App = () => {
